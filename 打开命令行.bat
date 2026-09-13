a8b4a80f6909ae4d831a719b16afb3dc61975f31
dzhuo801-spec <dzhuo801@gmail.com>
2026-09-13 15:23:23 +0800

    chore: 添加双击运行脚本 运行day1.bat / 打开命令行.bat

---
+++ 打开命令行.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "%~dp0"
+echo Working directory: %CD%
+echo Type:  py src\day1.py
+echo.
+cmd /k
---
+++ 运行day1.bat
@@ -0,0 +1,12 @@
+@echo off
+cd /d "%~dp0"
+echo ========================================
+echo  Running day1.py
+echo ========================================
+echo.
+py src\day1.py
+echo.
+echo ========================================
+echo  Done. Images are in the output folder.
+echo ========================================
+pause
